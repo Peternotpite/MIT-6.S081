c1343040786a7e369c46f23e9fc19aa8cc831ac2
郭瀚奇 <1479278024@qq.com>
2024-03-15 22:13:19 +0800

    message

--- user/pingpong.c
+++ user/pingpong.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
         //info同理
         char buf[2];
         char info[2]="a"; 
-        buf[2]=0;
+        buf[1]=0;
 
         if(write(p[1],info,1) != 1){
             fprintf(2,"Error,parent don't send pid\n");
@@ -45,55 +45,9 @@ int main(int argc, char *argv[])
         }
         close(p[0]);
         printf("%d: recived pong\n",pid);
+        exit(0);
     }
-    return 0;
 }
 
 
 
-// int main(int argc, char const *argv[])
-// {
-// 	int pid;
-// 	int p[2];
-// 	pipe(p);
-
-// 	if (fork() == 0) // child (receive -> send)
-// 	{
-// 		pid = getpid();
-// 		char buf[2];
-// 		if (read(p[0], buf, 1) != 1)
-// 		{
-// 			fprintf(2, "failed to read in child\n");
-// 			exit(1);
-// 		}
-// 		close(p[0]);
-// 		printf("%d: received ping\n", pid);
-// 		if(write(p[1], buf, 1) != 1)
-// 		{
-// 			fprintf(2, "failed to write in child\n");
-// 			exit(1);
-// 		}
-// 		close(p[1]);
-// 		exit(0);
-// 	}else{			// parent (send -> receive)
-// 		pid = getpid();
-// 		char info[2] = "a";
-// 		char buf[2];
-// 		buf[1] = 0;
-// 		if (write(p[1], info, 1) != 1)
-// 		{
-// 			fprintf(2, "failed to write in parent\n");
-// 			exit(1);
-// 		}
-// 		// wait for child to receive ping
-// 		close(p[1]);
-// 		wait(0);
-// 		if(read(p[0], buf, 1) != 1){
-// 			fprintf(2, "failed to read in parent\n");
-// 			exit(1);
-// 		}
-// 		printf("%d: received pong\n", pid);
-// 		close(p[0]);
-// 		exit(0);
-// 	}
-// }
