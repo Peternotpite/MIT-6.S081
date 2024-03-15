ea4576165682bb3fad1929325bb90e144e2a8e5a
郭瀚奇 <1479278024@qq.com>
2024-03-15 22:10:43 +0800

    message

--- user/pingpong.c
+++ user/pingpong.c
@@ -10,17 +10,17 @@ int main(int argc, char *argv[])
 
     if (fork() == 0) { 
         pid = getpid();
-        //得到pid后，pid会自动存入数组元素buf[0]中，pid通过ASCII码以字符存入buf
-        //（目前不知道怎么会自动存进去）
-        char buf[10];
+        //得到pid后，pid会一直存放在管道的输出缓冲区，
+        //"1"只是为了说明进程能够正确读取管道的信息
+        char buf[2];
         //printf("sizeof(pid)=：%d",sizeof(pid));
-        if(read(p[0],buf,8) != 8){
+        if(read(p[0],buf,1) != 1){
             fprintf(2,"Error,child don't get pid\n");
             exit(1);
         }
         close(p[0]);
         printf("%d: recived ping\n",pid);
-        if(write(p[1],buf,8) != 8){
+        if(write(p[1],buf,1) != 1){
             fprintf(2,"Error,child don't send pid\n");
             exit(1);
         }
@@ -29,17 +29,17 @@ int main(int argc, char *argv[])
     }else{
         pid=getpid(); 
         //info同理
-        char buf[10];
-        char info[10]="a"; 
-        buf[9]=0;
+        char buf[2];
+        char info[2]="a"; 
+        buf[2]=0;
 
-        if(write(p[1],info,8)!=8){
+        if(write(p[1],info,1) != 1){
             fprintf(2,"Error,parent don't send pid\n");
             exit(1);
         }
         close(p[1]);
         wait(0);
-        if(read(p[0],buf,8)!=8){
+        if(read(p[0],buf,1) != 1){
             fprintf(2,"Error,parent don't get pid\n");
             exit(1);
         }
@@ -48,3 +48,52 @@ int main(int argc, char *argv[])
     }
     return 0;
 }
+
+
+
+// int main(int argc, char const *argv[])
+// {
+// 	int pid;
+// 	int p[2];
+// 	pipe(p);
+
+// 	if (fork() == 0) // child (receive -> send)
+// 	{
+// 		pid = getpid();
+// 		char buf[2];
+// 		if (read(p[0], buf, 1) != 1)
+// 		{
+// 			fprintf(2, "failed to read in child\n");
+// 			exit(1);
+// 		}
+// 		close(p[0]);
+// 		printf("%d: received ping\n", pid);
+// 		if(write(p[1], buf, 1) != 1)
+// 		{
+// 			fprintf(2, "failed to write in child\n");
+// 			exit(1);
+// 		}
+// 		close(p[1]);
+// 		exit(0);
+// 	}else{			// parent (send -> receive)
+// 		pid = getpid();
+// 		char info[2] = "a";
+// 		char buf[2];
+// 		buf[1] = 0;
+// 		if (write(p[1], info, 1) != 1)
+// 		{
+// 			fprintf(2, "failed to write in parent\n");
+// 			exit(1);
+// 		}
+// 		// wait for child to receive ping
+// 		close(p[1]);
+// 		wait(0);
+// 		if(read(p[0], buf, 1) != 1){
+// 			fprintf(2, "failed to read in parent\n");
+// 			exit(1);
+// 		}
+// 		printf("%d: received pong\n", pid);
+// 		close(p[0]);
+// 		exit(0);
+// 	}
+// }
